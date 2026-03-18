f6da808af80b7fea991d4fb703a36d8a5482f74e
Titiss <Mathis.fcs.lav@gmail.com>
2026-03-18 09:57:12 +0100

    portfolio vide

--- htdocs/app/Config/Routes.php
+++ htdocs/app/Config/Routes.php
@@ -1,12 +1,8 @@
 <?php
+
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Portfolio::index');
-$routes->get('tableau', 'Portfolio::tableau');
-$routes->get('projets', 'Portfolio::projets');
-
-$routes->get('contact', 'Contact::index');
-$routes->post('contact/reponse', 'Contact::reponse');
+$routes->get('/', 'Home::index');
--- htdocs/app/Helpers/age_helper.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!function_exists('age')) {
-    function age($birthDate) {
-        $birthDate = new DateTime($birthDate);
-        $today = new DateTime();
-        return $today->diff($birthDate)->y;
-    }
-}
